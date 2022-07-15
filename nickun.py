268eba13820420dca35612bed85c0b6c8da28a90
Gunner96 <bhaichungtamang@gmail.com>
2022-07-15 15:53:58 +0530

    Orientation independence has been achieved, thumb is still an issue

--- nickun.py
+++ nickun.py
@@ -15,11 +15,12 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-# finger_val = [(4, 2), (8, 6), (12, 10), (16, 14), (20, 18)]
-finger_val = [(4, 5), (8, 5), (12, 9), (16, 13), (20, 17)]
+finger_val = [(4, 2), (8, 6), (12, 10), (16, 14), (20, 18)]
+# finger_val = [(4, 5), (8, 5), (12, 9), (16, 13), (20, 17)]
 status_left = [-1 for _ in finger_val]
 status_right = [-1 for _ in finger_val]
-
+trigger_command = [[0],[0]]
+default_command = [0 for val in range(10)]
 
 def calculate(upper, lower):
     print("from calculate",upper,lower)
@@ -38,9 +39,16 @@ def check_left_right(cord):
         return 0
     return 1
 
-def thumb(fing,coordinate,side):
-    tip = coordinate[fing[0]][0]
-    base = coordinate[fing[1]][0]
+def thumb(fing,coordinate,side,orient):
+    axis = 0
+    upper = 0
+    lower = 1
+    if orient == "upside":
+        axis = 1
+        upper = 1
+        lower = 0
+    tip = coordinate[fing[upper]][axis]
+    base = coordinate[fing[lower]][axis]
     if side == "left":
         if tip > base:
             return 0
@@ -49,14 +57,37 @@ def thumb(fing,coordinate,side):
         return 0
     return 1
 
+def checkUp(fing , coordinate,orient):
+    axis = 1
+    upper = 0
+    lower = 1
+    if orient == "upside":
+        x = 0
+        upper = 1
+        lower = 0
+    tip = coordinate[fing[upper]][axis]
+    base = coordinate[fing[lower]][axis]
+    # distance = calculate(tip, base)
+    # if distance > 25:
+    #     return 0
+    # return 1
+    if tip > base:
+        return 1
+    return 0
 
-def checkUp(fing , coordinate):
-    tip = coordinate[fing[0]]
-    base = coordinate[fing[1]]
-    distance = calculate(tip, base)
-    if distance > 25:
-        return 0
-    return 1
+# def checkUp(fing , coordinate,orient):
+#     x = 1
+#     if orient == "upside":
+#         x = 0
+#     tip = coordinate[fing[0]][x]
+#     base = coordinate[fing[1]][x]
+#     # distance = calculate(tip, base)
+#     # if distance > 25:
+#     #     return 0
+#     # return 1
+#     if tip > base:
+#         return 1
+#     return 0
 
 
 
@@ -89,31 +120,25 @@ while cap.isOpened():
                     h, w, c = img.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
                     coordinate[id] = (cx, cy)
-                # and coordinate[20][0] > mid_point
-                #hand_side
                 handle = checkOrientation(coordinate)
-                if hand == 'right':
-                    print("Hand shown right")
-                elif hand == 'left':
-                    print("hand shown left")
 
-                if handle == 'right':
+                if handle[0] == 'right':
                     for idx, val in enumerate(finger_val):
-                        # if idx == 0:
-                        #     status_right[idx] = thumb(val, coordinate, side="right")
-                        # else:
-                        #     status_right[idx] = checkUp(val, coordinate)
-                        status_right[idx] = checkUp(val,coordinate)
+                        if idx == 0:
+                            status_right[idx] = thumb(val, coordinate, side="right",orient=handle[1])
+                        else:
+                            status_right[idx] = checkUp(val, coordinate,orient=handle[1])
+                        # status_right[idx] = checkUp(val,coordinate)
 
                     # cv2.putText(img, "right" + str(status_right), (70, 450), cv2.FONT_HERSHEY_PLAIN, 3,
                     #                 (255, 0, 255), 2)
                 #
-                elif handle == 'left':
+                elif handle[0] == 'left':
                     for idx, val in enumerate(finger_val):
                         if idx == 0:
-                            status_left[idx] = thumb(val, coordinate, side="left")
+                            status_left[idx] = thumb(val, coordinate, side="left",orient=handle[1])
                         else:
-                            status_left[idx] = checkUp(val, coordinate)
+                            status_left[idx] = checkUp(val, coordinate,orient=handle[1])
                     status_left.reverse()
                     # cv2.putText(img, "left" + str(status_left), (70, 400), cv2.FONT_HERSHEY_PLAIN, 3,
                     #             (255, 0, 255), 2)
@@ -121,10 +146,21 @@ while cap.isOpened():
     cv2.putText(img, "Command" + str(status_left+status_right), (0, 400), cv2.FONT_HERSHEY_PLAIN, 2,
                 (255, 0, 255), 2)
     comb = status_left+status_right
-    command = [1, 1, 1, 0, 0, 1, 1, 1]
-    t1 = None
-    if not t1 and comb == command:
-        openBrowser()
+    command = [1, 1, 1, 0,1,1, 0, 1, 1, 1]
+    #TRIGGER SIGNAL
+    if comb != trigger_command[1]:
+        trigger_command[0] = trigger_command[1]
+        trigger_command[1] = comb
+
+    # cv2.putText(img, "Command" + str(trigger_command[0]) + str(trigger_command[1]), (0, 400), cv2.FONT_HERSHEY_PLAIN, 2,
+    #             (255, 0, 255), 2)
+    if trigger_command[0] != trigger_command[1] and trigger_command[1] == default_command:
+        if trigger_command[0] == command:
+            openBrowser()
+
+
+
+
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
--- orientation.py
+++ orientation.py
@@ -2,7 +2,7 @@ def checkOrientation(landmarks_list1):
     if landmarks_list1[3][0] > landmarks_list1[17][0]:
         """ when x-coordinate of 3index(thumb)is greater than 17th index(pinky) then its left"""
         if (landmarks_list1[0][1] > landmarks_list1[12][1]):
-            return "left"
+            return ("left","downside")
             # but only if it is upright i.e y-coordinate of 12th index is less than 0th index(palm)
             # cv2.rectangle(img, (20, 335), (200, 425), (0, 255, 0), cv2.FILLED)
             # cv2.putText(img, "Left", (landmarks_list1[0][1], landmarks_list1[1][2]), cv2.FONT_HERSHEY_SIMPLEX, 2,
@@ -10,7 +10,7 @@ def checkOrientation(landmarks_list1):
             # Mapping.mappingGestureUpright(landmarks_list1, landmarks_list2)
         else:
             """else it will be right hand properties which is upside down"""
-            return "right"
+            return ("right","upside")
             # cv2.rectangle(img, (20, 335), (200, 425), (0, 255, 0), cv2.FILLED)
             # cv2.putText(img, "Right", (landmarks_list1[0][1], landmarks_list1[1][2]), cv2.FONT_HERSHEY_SIMPLEX, 2,
             #             (255, 0, 0), 5)
@@ -18,14 +18,14 @@ def checkOrientation(landmarks_list1):
     else:
         if (landmarks_list1[0][1] > landmarks_list1[12][1]):
             """#upright left hand 1"""
-            return "right"
+            return ("right","downside")
             # cv2.rectangle(img, (20, 335), (200, 425), (0, 255, 0), cv2.FILLED)
             # cv2.putText(img, "Right", (landmarks_list1[0][1], landmarks_list1[1][2]), cv2.FONT_HERSHEY_SIMPLEX, 2,
             #             (255, 0, 0), 5)
             # Mapping.mappingGestureUpright(landmarks_list1, landmarks_list2)
         else:
             """#upside down right hand 1"""
-            return "left"
+            return ("left","upside")
             # cv2.rectangle(img, (20, 335), (200, 425), (0, 255, 0), cv2.FILLED)
             # cv2.putText(img, "Left", (landmarks_list1[0][1], landmarks_list1[1][2]), cv2.FONT_HERSHEY_SIMPLEX, 2,
             #             (255, 0, 0), 5)
